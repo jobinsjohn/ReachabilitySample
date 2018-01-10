adbf78a34a728574aa4df3c6a2224aa367f0875c
Jobins John <jobins666@gmail.com>
2018-01-10 15:45:56 +0400

    Fixed Typos

--- Reachability Sample/ViewController.swift
+++ Reachability Sample/ViewController.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2018 Jobins John. All rights reserved.
 //
 
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -17,18 +16,13 @@ class ViewController: UIViewController {
     // MARK: - View Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         NotificationCenter.default.addObserver(self, selector: #selector(reachabilityDidChange(_:)), name: NSNotification.Name(rawValue: ReachabilityDidChangeNotificationName), object: nil)
-        
         _ = reachabilityObj?.startNotifier()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
         checkReachability()
-        
     }
     
     deinit {
@@ -36,29 +30,21 @@ class ViewController: UIViewController {
         reachabilityObj?.stopNotifier()
     }
     
-    // MARk: - Methods
+    // MARK: - Methods
     func checkReachability() {
-        
         guard let r =  reachabilityObj else {
             return
         }
-        
         if r.isReachable {
-            
             view.backgroundColor = UIColor.green
-            
         } else {
-            
             view.backgroundColor = UIColor.red
         }
     }
     
     @objc func reachabilityDidChange(_ notification: Notification) {
-        
         checkReachability()
-        
     }
-    
 }
 
 
