1b0d05b365497176b0448c6d4f486b9e75dd9d2a
Jobins John <jobins666@gmail.com>
2018-01-10 15:37:43 +0400

    first commit

---
+++ Reachability Sample/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Reachability Sample
+//
+//  Created by Jobins John on 1/10/18.
+//  Copyright © 2018 Jobins John. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    var reachabilityObj = MyReachability.networkReachabilityForInternetConnection()
+    
+    // MARK: - View Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(reachabilityDidChange(_:)), name: NSNotification.Name(rawValue: ReachabilityDidChangeNotificationName), object: nil)
+        
+        _ = reachabilityObj?.startNotifier()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        checkReachability()
+        
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+        reachabilityObj?.stopNotifier()
+    }
+    
+    // MARk: - Methods
+    func checkReachability() {
+        
+        guard let r =  reachabilityObj else {
+            return
+        }
+        
+        if r.isReachable {
+            
+            view.backgroundColor = UIColor.green
+            
+        } else {
+            
+            view.backgroundColor = UIColor.red
+        }
+    }
+    
+    @objc func reachabilityDidChange(_ notification: Notification) {
+        
+        checkReachability()
+        
+    }
+    
+}
+
+
